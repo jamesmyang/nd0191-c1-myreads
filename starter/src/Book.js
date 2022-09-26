d79f7770727af7af554b147ab628992cdad9b6df
James Yang <jamesmyang@hotmail.com>
2022-09-26 17:51:11 -0400

    Add more javascripts

---
+++ starter/src/Book.js
@@ -0,0 +1,37 @@
+const Book = ({ book }) => {
+  const img = book.imageLinks.thumbnail;
+  console.log(img);
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 188,
+            backgroundImage:
+              `url(${book.imageLinks.thumbnail})`,
+          }}
+        >
+        </div>
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">
+              Currently Reading
+            </option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.authors[0]}</div>
+    </div>
+  );
+}
+
+export default Book;
