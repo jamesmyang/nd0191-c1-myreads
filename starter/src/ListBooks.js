f5c46b1746c9a60d54e3dc3ee91ea24cb4140b5c
James Yang <jamesmyang@hotmail.com>
2022-09-28 10:25:37 -0400

    Link BooksAPI

--- starter/src/ListBooks.js
+++ starter/src/ListBooks.js
@@ -1,98 +1,48 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import BookShelf from "./BookShelf";
+import * as BooksAPI from "./BooksAPI";
 
 const ListBooks = () => {
-
-  const book = {
-    title: "Ender's Game",
-    authors: ["Orson Scott Card", "unknown 1", "unknown 2"],
-    imageLinks: {
-      smallThumbnail: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-      thumbnail: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-    },
-    id: "yDtCuFHXbAYC",
-    shelf: "currentlyReading"
-  }
-
-  const book11 = {
-    title: "1776",
-    authors: ["David McCullough"],
-    imageLinks: {
-      smallThumbnail: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-      thumbnail: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
-    },
-    id: "uu1mC6zWNTwC",
-    shelf: "wantToRead"
-  }
-
-  const book12 = {
-    title: "Harry Potter and the Sorcerer's Stone",
-    authors: ["J.K. Rowling"],
-    imageLinks: {
-      smallThumbnail: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-      thumbnail: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
-    },
-    id: "uu1mC6zWNTwC",
-    shelf: "wantToRead"
-  }
-
-  const book21 = {
-    title: "The Hobbit",
-    authors: ["J.R.R. Tolkien"],
-    imageLinks: {
-      smallThumbnail: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
-      thumbnail: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
-    },
-    id: "pD6arNyKyi8C",
-    shelf: "read"
-  }
-
-  const book22 = {
-    title: "Oh, the Places You'll Go!",
-    authors: ["Seuss"],
-    imageLinks: {
-      smallThumbnail: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
-      thumbnail: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"
-    },
-    id: "1q_xAwAAQBAJ",
-    shelf: "read"
-  }
-
-  const book23 = {
-    title: "The Adventures of Tom Sawyer",
-    authors: ["Mark Twain"],
-    imageLinks: {
-      smallThumbnail: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
-      thumbnail: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"
-    },
-    id: "32haAAAAMAAJ",
-    shelf: "read"
-  }
-
   const [reading, setReading] = useState([]);
   const [want, setWant] = useState([]);
   const [read, setRead] = useState([]);
 
   useEffect(() => {
-    let books = [book, book11, book12, book21, book22, book23];
-    resort(books);
+    let mounted = true;
+
+    const getBooks = async () => {
+      const books = await BooksAPI.getAll();
+
+      if (books) {
+        if (mounted) {
+          resort(books);
+        }
+      }
+    };
+
+    getBooks();
+
+    return () => {
+      mounted = false;
+    };
   }, []);
 
   const handleMove = (book, shelf) => {
-    /**
-     * update server for book
-     */
-
     book.shelf = shelf;
 
+    const update = async () => {
+      const res = await BooksAPI.update(book, shelf);
+    }
+
+    update();
     resort(reading.concat(want).concat(read));
   };
 
   const resort = (books) => {
-    setReading(books.filter(book => book.shelf === "currentlyReading"));
-    setWant(books.filter(book => book.shelf === "wantToRead"));
-    setRead(books.filter(book => book.shelf === "read"));
+    setReading(books.filter(book => book.imageLinks.thumbnail !== undefined && book.shelf === "currentlyReading"));
+    setWant(books.filter(book => book.imageLinks.thumbnail !== undefined && book.shelf === "wantToRead"));
+    setRead(books.filter(book => book.imageLinks.thumbnail !== undefined && book.shelf === "read"));
   }
 
   return (
